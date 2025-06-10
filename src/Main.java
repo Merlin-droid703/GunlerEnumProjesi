c9a695eececf330d9c65102eccb2958e3bd98640
Merlin-droid703 <melisa_aladag@hotmail.com>
2025-06-10 16:17:21 +0300

    "Yarın değişkeni eklendi ve yorumlar güncellendi"

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 // Main.java
 public class Main {
+
     public static void main(String[] args) {
-        // Gun enum'ından bir değişken tanımlıyoruz.
+        // 1. Gun enum'ından bir değişken tanımlıyoruz.
+        // Bu değişken sadece Gun enum'ındaki değerlerden birini alabilir.
         Gun bugun = Gun.CUMA;
 
-        // Ekrana "Bugün günlerden: CUMA" yazdırıyoruz.
+        // 2. Ekrana "Bugün günlerden: CUMA" yazdırıyoruz.
+        // 'bugun' değişkeni yazdırılırken Java, enum sabitinin adını ("CUMA") kullanır.
         System.out.println("Bugün günlerden: " + bugun);
+
+        // 3. Ekrana "Yarın günlerden: Cumartesi" yazdırıyoruz.
+        Gun yarin = Gun.CUMARTESI;
+        System.out.println("Yarın günlerden: " + yarin);
     }
 }
